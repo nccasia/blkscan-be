a168e917ed639e85473f03c6ae3f9a5f80decf41
hai.ngodang <hai.ngodang@ncc.asia>
2022-10-27 10:49:14 +0700

    implement neo4j

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +7,8 @@ import { TagsModule } from './modules/tags/tags.module';
 import { WalletsModule } from './modules/wallets/wallets.module';
 import { configEnvPath } from './common/helper/env.helper';
 import { TypeOrmConfigSerivce } from './shared/typeorm/typeorm.service';
+import { Neo4jModule } from '@nhogs/nestjs-neo4j';
+import { Neo4jConfig } from '@nhogs/nestjs-neo4j/dist';
 
 @Module({
   imports: [
@@ -14,6 +16,19 @@ import { TypeOrmConfigSerivce } from './shared/typeorm/typeorm.service';
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigSerivce }),
     TagsModule,
     WalletsModule,
+    Neo4jModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): Neo4jConfig => ({
+        scheme: configService.get('NEO4J_SCHEME'),
+        host: configService.get('NEO4J_HOST'),
+        port: configService.get('NEO4J_PORT'),
+        username: configService.get('NEO4J_USERNAME'),
+        password: configService.get('NEO4J_PASSWORD'),
+        database: configService.get('NEO4J_DATABASE'),
+      }),
+      global: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
