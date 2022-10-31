dfc3c2558b596bdb64d5f956f1658ae610f2ebec
hai.ngodang <hai.ngodang@ncc.asia>
2022-10-31 11:39:38 +0700

    implement @relationship Directive

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,10 +12,13 @@ import { Neo4jConfig } from '@nhogs/nestjs-neo4j/dist';
 import { SendModule } from './modules/send/send.module';
 import { AddressModule } from './modules/address/address.module';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriverConfig } from '@nestjs/apollo';
-import { ApolloDriver } from '@nestjs/apollo/dist/drivers';
-import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
+// import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
 import { join } from 'path';
+import { Neo4jGraphQL } from '@neo4j/graphql';
+import { generate, OGM } from '@neo4j/graphql-ogm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { readFileSync } from 'fs';
+import neo4j from 'neo4j-driver';
 
 @Module({
   imports: [
@@ -36,19 +39,64 @@ import { join } from 'path';
       }),
       global: true,
     }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   typePaths: ['./**/*.graphql'],
+    //   transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
+    //   debug: false,
+    //   playground: true,
+    //   // plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    //   definitions: {
+    //     path: join(process.cwd(), 'src/gen/graphql.schema.ts'),
+    //     emitTypenameField: true,
+    //     // outputAs: 'class',
+    //   },
+    //   // include: [AddressModule],
+    // }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
-      transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
-      debug: false,
-      playground: true,
-      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      definitions: {
-        path: join(process.cwd(), 'src/gen/graphql.schema.ts'),
-        emitTypenameField: true,
-        // outputAs: 'class',
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const SCHEMA = './src/index.graphql';
+        const typeDefs = readFileSync(SCHEMA, 'utf-8');
+        // TODO: get driver from Neo4jModule
+        const driver = neo4j.driver(
+          `${configService.get('NEO4J_SCHEME')}://${configService.get(
+            'NEO4J_HOST',
+          )}:${configService.get('NEO4J_HOST')}`,
+          neo4j.auth.basic(
+            configService.get('NEO4J_USERNAME'),
+            configService.get('NEO4J_PASSWORD'),
+          ),
+        );
+        const ogm = new OGM({ typeDefs, driver });
+        await generate({
+          ogm,
+          outFile: join(process.cwd(), 'src/gen/graphql.schema.ts'),
+        });
+        // process.exit(1);
+        await ogm.init();
+
+        // const resolvers = {
+        //   Query: {
+        //     yourCustomerResolverName: async (_source, { name }) => {
+        //       const extractedType = 'Person';
+        //       ogm.model(extractedType);
+        //       const companies = [];
+        //       return companies;
+        //     },
+        //   },
+        // };
+
+        const neo4jGraphQL = new Neo4jGraphQL({ typeDefs, driver });
+        const schema = await neo4jGraphQL.getSchema();
+
+        return {
+          schema,
+          playground: true,
+        };
       },
-      // include: [AddressModule],
     }),
     SendModule,
     AddressModule,
