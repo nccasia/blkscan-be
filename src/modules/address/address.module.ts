b110b9127cecb88cbf6e2768ecad6da7eb317e66
haingodangncc <116351745+haingodangncc@users.noreply.github.com>
2022-11-01 10:04:47 +0700

    Feature/integrate graphql (#9)
    * integrate graphql
    
    * implement @relationship Directive
    
    * implement directives
    
    * add log

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SendModule } from '../send/send.module';
+import { AddressResolver } from './address.resolver';
 import { AddressService } from './address.service';
 
 @Module({
   imports: [SendModule],
   // controllers: [AddressController],
-  providers: [AddressService],
-  exports: [AddressService],
+  providers: [AddressService, AddressResolver],
+  exports: [AddressService, AddressResolver],
 })
 export class AddressModule {}
